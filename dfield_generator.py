fa05a0056e60264094198fb9570f930a1876583c
donglai96 <donglaima96@gmail.com>
2021-01-14 23:29:28 -0800

    animation

--- dfield_generator.py
+++ dfield_generator.py
@@ -29,7 +29,7 @@ def B(r, theta, alpha, B0):
 
 class DipoleMoment(object):
 
-    def __init__(self, name, position, alpha, B0):
+    def __init__(self, name, position, alpha, B0,outputinfo = True):
         """
 
         Args:
@@ -42,11 +42,11 @@ class DipoleMoment(object):
         self.position = position
         self.alpha = alpha
         self.B0 = B0
-
-        print("This is No. {} field ".format(self.name))
-        print("The initial position is :", position)
-        print("The angle is :", alpha)
-        print("The constant(strength) of this dipole field is:", B0)
+        if outputinfo:
+            print("This is No. {} field ".format(self.name))
+            print("The initial position is :", position)
+            print("The angle is :", alpha)
+            print("The constant(strength) of this dipole field is:", B0)
 
         self.bfield = None
 
@@ -69,3 +69,41 @@ class DipoleMoment(object):
         Bx = -Btheta * s + Br * c
         By = Btheta * c + Br * s
         return Bx, By
+
+
+def get_field_t(t):
+    a = np.sin(t)
+    k,m,n,i = 50,50,5,60
+
+    x0 = -1000
+    y0 = 0
+    theta0 = -30.6
+    s0 = 62
+
+
+    x = x0 + k*a
+    y = y0
+    theta = theta0 + n*a
+    s = s0 + i*a
+
+    polar1 = DipoleMoment(name = 1, position = [500,0],
+                             alpha= np.radians(9.6),
+                             B0 = 5,outputinfo=False)
+
+    polar2 = DipoleMoment(name = 2, position = [x,y],
+                             alpha= np.radians(theta),
+                             B0 = s,outputinfo=False)
+
+
+
+    nx, ny = 500, 500
+    XMAX, YMAX = 500, 500
+    xx = np.linspace(-XMAX, XMAX, nx)
+    yy = np.linspace(-YMAX, YMAX, ny)
+    X, Y = np.meshgrid(xx, yy)
+    Bx_1, By_1 = polar1.get_field(X,Y)
+
+
+    Bx_2, By_2 = polar2.get_field(X,Y)
+    Bx, By = Bx_1 + Bx_2, By_1 + By_2
+    return Bx, By
